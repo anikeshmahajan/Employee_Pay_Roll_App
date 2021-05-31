54b0eb5a1e9c287dd3ec262d090683ab22df0003
Anikesh <anikesh@0725@gmail.com>
2021-05-31 20:29:30 +0530

    Updation

--- JavaScript/empObj.js
+++ JavaScript/empObj.js
@@ -1,40 +1,25 @@
-const salary = document.querySelector('#salary');
-const output = document.querySelector('.Salary-output');
-output.textContent = salary.value;
-salary.addEventListener('input', function() {
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function() {
+        if (name.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayRoll()).name = name.value;
+            textError.textContent = "";
+        } catch (e) {
+            textError.textContent = e;
+        }
+    });
+
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.Salary-output');
     output.textContent = salary.value;
+    salary.addEventListener('input', function() {
+        output.textContent = salary.value;
+    });
+
 });
-
-
-const text = document.querySelector('#name');
-const textError = document.querySelector(".text-error");         
-text.addEventListener('input',function(){
-    let nameRegex = RegExp("^[A-Z]{1}[a-zA-Z0-9\\s]{2,}$");
-    if(nameRegex.test(text.value)) textError.textContent= " ";
-    else textError.textContent="Name is Incorrect";
-});
-
-
-
-
-const form=document.getElementById('form');
-
-form.addEventListener('submit',function(event)
-{
-    event.preventDefault();
-    var name = document.getElementById('name').value;
-    var profile = document.querySelector('input[name = profile]:checked').value;
-     var gender = document.querySelector('input[name = gender]:checked').value;
-     var department = document.querySelector('input[type = checkbox]:checked').value;
-     var salary = document.getElementById("salary").value;
-     var notes = document.getElementById("notes").value;
-     var startDate = new Date(parseInt(document.getElementById("year").value), parseInt(document.getElementById("month").value), parseInt(document.getElementById("day").value));
-
-     console.log("Name : "+name + "\n Profile :" +profile + "\n Gender :" +gender +
-     "\n Depart :" + department + "\n Salary :"+ salary + "\n Notes : "+ notes
-     + "StartDate :"+ startDate )
-
-    
-}); 
-
 
