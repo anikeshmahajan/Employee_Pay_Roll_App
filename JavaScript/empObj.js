cfd26800c6e60d6e6a9adbf5f895acd9b69b59a1
Anikesh <anikesh@0725@gmail.com>
2021-05-31 20:50:09 +0530

    Addition : Ability to create object on onSave

--- JavaScript/empObj.js
+++ JavaScript/empObj.js
@@ -15,11 +15,58 @@ window.addEventListener('DOMContentLoaded', (event) => {
     });
 
     const salary = document.querySelector('#salary');
-    const output = document.querySelector('.Salary-output');
+    const output = document.querySelector('.salary-output');
     output.textContent = salary.value;
     salary.addEventListener('input', function() {
         output.textContent = salary.value;
     });
 
 });
+
+
+const save = () => {
+    try {
+        let employeeData = createEmployeepayRoll();
+    } catch (e) {
+        return
+    }
+}
+
+const createEmployeepayRoll = () => {
+    let employeePayroll = new EmployeePayRoll();
+    try {
+        employeePayroll.name = getInputValueById('#name');
+    } catch (e) {
+        setTextValue('.text-error', e);
+        throw e;
+    }
+    employeePayroll.picture = getSelectedValues('[name=profile]').pop();
+    employeePayroll.gender = getSelectedValues('[name=gender]').pop();
+    employeePayroll.department = getSelectedValues('[name=department]');
+    employeePayroll.salary = getInputValueById('#salary');
+    employeePayroll.notes = getInputValueById('#notes');
+    let date = getInputValueById('#day') + " " + getInputValueById('#month') + " " + getInputValueById('#year');
+    employeePayroll.date = Date.parse(date);
+    alert(employeePayroll.toString());
+    return employeePayroll;
+}
+
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let setItems = [];
+    allItems.forEach(item => {
+        if (item.checked) setItems.push(item.value);
+    });
+    return setItems;
+}
+
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+const getInputElementValue = (id) => {
+    let value = document.getElementById(id).value;
+    return value;
+}
 
